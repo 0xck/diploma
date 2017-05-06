803f9df3e6e190e140a7ba0c07142a7d9a040434
Constantine Kormashev <constantine.kormashev@gmail.com>
2017-05-06 17:55:26 +0300

    config.py from config generator

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-import os
-# DB conf
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+SQLALCHEMY_DATABASE_URI = "sqlite///db.sqlite"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-# redis
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-# flask
-app_listen = '0.0.0.0'
-app_port = '5000'
+app_listen = "0.0.0.0"
+app_port = "5000"
+redis_url = "redis://localhost:6379"
