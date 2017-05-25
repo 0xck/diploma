75a0ef1f7ac3614bc4fd24357dae07dd4af0fcc9
Constantine Kormashev <constantine.kormashev@gmail.com>
2017-05-25 20:18:29 +0300

    added nav bar js

---
+++ app/static/js/nav_bar.js
@@ -0,0 +1,12 @@
+'use strict';
+$(document).ready(function() {
+    if ($('h1').hasClass('tasks')) {
+        $('li.tasks_menu').addClass('active');
+    } else if ($('h1').hasClass('tests')) {
+        $('li.tests_menu').addClass('active');
+    } else if ($('h1').hasClass('trexes')) {
+        $('li.trexes_menu').addClass('active');
+    } else if ($('h1').hasClass('devices')) {
+        $('li.devices_menu').addClass('active');
+    };
+});
