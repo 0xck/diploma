20a70470e38ea658aaa1cc5046c037bd78f33aca
Constantine Kormashev <constantine.kormashev@gmail.com>
2017-12-06 18:54:00 +0300

    old moved

--- exceptions.py
+++
@@ -1,9 +0,0 @@
-# especial exception for making deal with signals
-class GracefulExit(Exception):
-    # just making own exception class
-    pass
-
-
-def signal_handler(signum, frame):
-    # raises exception
-    raise GracefulExit()
